246fae9e577d1f68ea38a0b8d4fef3c300972883
Tom Scott <tomscott87@gmail.com>
2022-12-20 21:07:44 +0000

    Update newday command to create main_test.go

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # aoc2022
 Advent of Code 2022
+
+## Generating boilerplate
+
+To generate a main.go and main_test.go for a new day. Run the following:
+```
+go run cmd/newday/main.go -day 4 -part 1
+```
--- cmd/newday/main.go
+++ cmd/newday/main.go
@@ -7,6 +7,46 @@ import (
 	"os"
 )
 
+var mainFile = `package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/tscott0/aoc2022/internal/readers"
+)
+
+func main() {
+	fmt.Println("Day %d part %d")
+
+	answer := readers.ReadAll("input/%02d.txt")
+	fmt.Printf("answer: %%d\n", solve(answer))
+}
+
+func solve(input string) (total int) {
+	lines := strings.Split(input, "\n")
+
+	for _, l := range lines {
+		fmt.Println(l)
+	}
+
+	return total
+}`
+
+var mainTestFile = `package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve(t *testing.T) {
+	s := solve("")
+
+	assert.Equal(t, 157, s)
+}`
+
 func main() {
 	day := flag.Int("day", 0, "day number (1-25)")
 	part := flag.Int("part", 0, "part number (1 or 2)")
@@ -23,16 +63,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	boilerplate := fmt.Sprintf(`package main
+	m := fmt.Sprintf(mainFile, *day, *part, *day)
+	if err := os.WriteFile(dir+"/main.go", []byte(m), 0644); err != nil {
+		log.Fatal(err)
+	}
 
-import "fmt"
-
-func main() {
-	fmt.Println("Day %d part %d")
-}
-`, *day, *part)
-
-	if err := os.WriteFile(dir+"/main.go", []byte(boilerplate), 0644); err != nil {
+	if err := os.WriteFile(dir+"/main_test.go", []byte(mainTestFile), 0644); err != nil {
 		log.Fatal(err)
 	}
 }
